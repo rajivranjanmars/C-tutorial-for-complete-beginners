b74d9da2b3669c577bfe977471c9070e7c4d2797
rajivranjanmars <67179338+rajivranjanmars@users.noreply.github.com>
2021-02-19 18:10:47 +0530

    Add files via upload

---
+++ 17_while_loops.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+  /*  while (true)
+    {
+        cout << "hello" <<endl;
+    } // this block of commented code wil program for infinite times
+    */
+
+
+   int i=0;
+    
+    while (i<=5)
+    {
+        /* code */
+        cout << " HELLO WORLD! is printed " << i +1<< " times . " << endl ;
+        i=i+1;
+    }
+    
+  cout << "program ended";
+
+ return 0;
+
+}
