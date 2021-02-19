135aa24961fc2ee67eeeb87aa296700d01cfefbd
rajivranjanmars <67179338+rajivranjanmars@users.noreply.github.com>
2021-02-19 20:42:02 +0530

    Add files via upload

---
+++ 20_for.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+using namespace std;
+int main()
+{
+        
+        for (int i = 0; i < 10; i++)
+        {
+            cout << "hello " << i+1 << endl; // i+1 because i start from 0
+
+        }
+                 
+ return 0;
+}
