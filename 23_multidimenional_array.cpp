ed927014f68ec7abf217bdd8a47a93cc4cff9026
rajivranjanmars <67179338+rajivranjanmars@users.noreply.github.com>
2021-02-23 19:37:03 +0530

    Add files via upload

---
+++ 23_multidimenional_array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+using namespace std;
+int main(){
+  string zoo[2][3]={
+                        {"peacock", "eagle" ,"parrot"},
+                        {"lion" , "tiger", "elephant"}
+
+                   };
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << zoo[i][j ]<< "     " ;
+        }
+        
+        cout << endl;
+    }
+    
+
+ return 0;
+}
